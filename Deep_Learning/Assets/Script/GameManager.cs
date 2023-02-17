f51722ffecc676b1b5cf6175d000f11c84e988b5
Shaadit <56261900+Shaadit@users.noreply.github.com>
2023-02-17 14:14:14 +0100

    Generalisation du code

--- Deep_Learning/Assets/Script/GameManager.cs
+++ Deep_Learning/Assets/Script/GameManager.cs
@@ -6,69 +6,86 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-enum Constant: int
+enum Constant
 {
-       Left = -1, // %size == 0 = bord
-       Right = 1, // %size == size-1 = bord
-       Top = 4, // +4 > size = bord
-       Bottom = -4,// -4 < 0 = bord
-       Stop = 0// -4 < 0 = bord
-   
+    Left,
+    Right,
+    Top,
+    Bottom,
+    Stop
 }
-
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] map;
-    [SerializeField] int playerPos; //position du joueur
+    [SerializeField] GameObject[] map;    
+    [SerializeField] GameObject Player; // Joueur
+    [SerializeField] int playerPos; // Joueur
+    [SerializeField] int mapWidth;
+    [SerializeField] int mapHeight;
+    [SerializeField] int victoryCase;
     [SerializeField] int[] rewards; // les recompenses
     [SerializeField] float[] valueInstantT;// valeur instant T
     [SerializeField] float[] valueT1;// instant t +1 
     [SerializeField] Constant[] action;// move foreach case
     [SerializeField] float gamma;
 
+    private int t_size_boucle;
+
     bool PolicyTrue = false;
-    
-    Constant[] values = { Constant.Bottom, Constant.Left, Constant.Right, Constant.Top, Constant.Stop };
+
+    private Dictionary<Constant, int> movements = new Dictionary<Constant, int>();
 
     // Start is called before the first frame update
     void Start()
     {
-        //map = new GameObject[16];
-        //reward = new int[16];
+        rewards = new int[map.Length];
+        valueInstantT = new float[map.Length];
+        valueT1 = new float[map.Length];
+        action = new Constant[map.Length];
+
+        t_size_boucle = map.Length;
+
+        movements.Add(Constant.Bottom, -mapWidth);
+        movements.Add(Constant.Top, mapWidth);
+        movements.Add(Constant.Right, 1);
+        movements.Add(Constant.Left, -1);
+        movements.Add(Constant.Stop, 0);
         
-        for(int i = 0; i < 16; i++)
+        for(int i = 0; i < t_size_boucle; i++)
         {
             rewards[i] = 0;
             
-
             valueT1[i] = 0;
 
             valueInstantT[i] = 0;
 
+            if (map[i].CompareTag("Obstacle"))
+            {
+                action[i] = Constant.Stop;
+                continue;
+            }
+            
             // 1 LEFT
             // 2 RIGHT
             // 3 TOP
             // 4 BOTTOM
             int index = Random.Range(0, 4);
-            Constant randomMove = (Constant)values.GetValue(index);
+            
+            Constant randomMove = (Constant)index;
             
             while (!IsPossible(randomMove, i))
             {
-                 index = Random.Range(0, 4);
-                randomMove = (Constant)values.GetValue(index);
+                index = Random.Range(0, 4);
+                randomMove = (Constant)index;
             }
 
             action[i] = randomMove;
-            if (i == 15)
-            {
-                rewards[i] = 1;
-            }
+
             Debug.Log($"Index : {i} pour move {randomMove} ");
         }
         playerPos = 0;
-        action[15] = Constant.Stop;
-
+        action[victoryCase] = Constant.Stop;
+        rewards[victoryCase] = 1;
     }
 
 
@@ -77,16 +94,16 @@ public class GameManager : MonoBehaviour
         switch (random)
         {
             case Constant.Top:
-                if (index + 4 > 15 || map[index + 4].CompareTag("Obstacle")) return false;
+                if (index + mapWidth > t_size_boucle-1 || map[index + mapWidth].CompareTag("Obstacle")) return false;
                 break;
             case Constant.Bottom:
-                if (index - 4 < 0 || map[index - 4].CompareTag("Obstacle")) return false;
+                if (index - mapWidth < 0 || map[index - mapWidth].CompareTag("Obstacle")) return false;
                 break;
             case Constant.Left:
-                if (index%4 == 0 || map[index - 1].CompareTag("Obstacle")) return false;
+                if (index%mapWidth == 0 || map[index - 1].CompareTag("Obstacle")) return false;
                 break;
             case Constant.Right:
-                if (index%4 == 3 || map[index + 1].CompareTag("Obstacle")) return false;
+                if (index%mapWidth == mapWidth-1 || map[index + 1].CompareTag("Obstacle")) return false;
                 break;
             default:
                 break;
@@ -105,7 +122,8 @@ public class GameManager : MonoBehaviour
     {
         try
         {
-            var actionToDo = (int)action[index];
+            if (map[index].CompareTag("Obstacle")) return 0;
+            var actionToDo = movements[action[index]];
             var reward = rewards[index + actionToDo];
             return reward;
         }
@@ -121,7 +139,7 @@ public class GameManager : MonoBehaviour
     {
         try
         {
-            var actionToDo = (int)movement;
+            var actionToDo = movements[movement];
             var reward = valueInstantT[index + actionToDo];
             return reward;
         }
@@ -139,9 +157,9 @@ public class GameManager : MonoBehaviour
         while( delta > 0.0001f)
         {
             delta = .0f;
-            for(int i =0;i< 16; i++)
+            for(int i =0;i< t_size_boucle; i++)
             {
-                valueT1[i] = CheckReward(i) + gamma * valueT1[i + (int)action[i]];
+                valueT1[i] = CheckReward(i) + gamma * valueT1[i + movements[action[i]]];
                 delta = MathF.Max(delta,Mathf.Abs(valueInstantT[i] - valueT1[i]));
                 valueInstantT = valueT1;
             }
@@ -150,16 +168,25 @@ public class GameManager : MonoBehaviour
 
     void PolicyImprovement()
     {
-        var temp = new Constant[15];
+        var temp = new Constant[t_size_boucle];
 
-        for (int i = 0; i < 15; i++)
+        for (int i = 0; i < t_size_boucle; i++)
         {
             temp[i] = action[i];
+            
+            if (i == victoryCase) continue;
+            
+            if (map[i].CompareTag("Obstacle"))
+            {
+                action[i] = Constant.Stop;
+                continue;
+            }
+            
             List<Constant> allActionForCase = new List<Constant>();
             
             for (int j = 0; j < 4; j++)
             {
-                Constant move = (Constant)values.GetValue(j);
+                Constant move = (Constant)j;
                 if (IsPossible(move, i)) allActionForCase.Add(move);
             }
 
@@ -179,8 +206,10 @@ public class GameManager : MonoBehaviour
 
         }
 
-        for (int i=0; i<15; i++)
+        for (int i=0; i<t_size_boucle; i++)
         {
+            if (i == victoryCase) continue;
+            
             if (action[i] != temp[i])
             {
                 PolicyTrue = false;
@@ -204,5 +233,22 @@ public class GameManager : MonoBehaviour
             }
             
         }
+        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            StartCoroutine(MovePlayer());
+        }
+    }
+
+    private IEnumerator MovePlayer()
+    {
+        while (playerPos != victoryCase)
+        {
+            yield return new WaitForSeconds(1);
+            var positionToGo = map[playerPos + movements[action[playerPos]]];
+            Player.transform.position = positionToGo.transform.position;
+            playerPos = Array.IndexOf(map, positionToGo);
+        }
+        Debug.Log("VICTOIRE");
     }
 }
